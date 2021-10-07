d2e18bc429a21f1b1c3c3a735226d8921c4a03fc
BrazhnikDA <65236303+BrazhnikDA@users.noreply.github.com>
2021-10-07 17:16:20 +0300

    Класс изменён,теперь он хранит список обьектов из json

--- Information.py
+++ Information.py
@@ -1,47 +1,53 @@
-import pandas as pd
-
+import pandas as pd             # Для работы с JSON
+from ObjectJson import Object   # Класс описывающий модель
 
 class Information:
-    # создание атрибутов класса
-    countLecturer = 0
-    lecturerCost = 0
+    # создание списка хранящий массив записей из JSON
+    ListObject = []
 
-    countStudents = 0
-    trainingCost = 0
+    # Геттеры
+    def getName(self, index):
+        obj = self.ListObject[index]
+        return obj.__getitem__(Object, 0)
 
-    countAccountant = 0
-    trainingAccountant = 0
+    def getCount(self, index):
+        obj = self.ListObject[index]
+        return obj.__getitem__(Object, 1)
 
-    internetCost = 0
+    def getSalary(self, index):
+        obj = self.ListObject[index]
+        return obj.__getitem__(Object, 2)
 
-    countSysAdmin = 0
-    sysAdminCost = 0
+    def getDescription(self, index):
+        obj = self.ListObject[index]
+        return obj.__getitem__(Object, 3)
 
+    # Спарсить данные с файла json, если ошибка выкидываем exception
     def ParseJson(self):
         try:
-            patients_df = pd.read_json("C:\\offline.json")
+            patients_df = pd.read_json("C:\\offline2.json")
             patients_df.head()
             return patients_df
         except Exception:
-            print("Файл с данными не удалось прочитать!", Exception)
+            print("Файл с данными не удалось прочитать!")
 
+    # Функция для начала работы класса (разбор полученных значений из json)
     def Start(self):
         offline = self.ParseJson()
 
         # Получения значения из JSON по названию ключа
-        countLecturer = offline['CountLecturer'].values
-        lecturerCost = offline['LecturerCost'].values
+        try:
+            name = offline["Name"].values
+            count = offline["Count"].values
+            salary = offline["Salary"].values
+            descrip = offline["Description"].values
+        except Exception:
+            print("Файл json не содержит указанный ключ!")
+            return
 
-        countStudents = offline['CountStudents'].values
-        trainingCost = offline['TrainingCost'].values
+        # Заполняем список обьектов, полученными данными
+        for i in range(name.size):
+            self.ListObject.append(Object(name[i], count[i], salary[i], descrip[i]))
+            i += 1
 
-        countAccountant = offline['CountAccountant'].values
-        trainingAccountant = offline['TrainingAccountant'].values
-
-        internetCost = offline['InternetCost'].values
-
-        countSysAdmin = offline['CountSysAdmin'].values
-        sysAdminCost = offline['SysAdminCost'].values
-
-        methodologistCost = offline['MethodologistCost'].values
-
+        return self.ListObject
