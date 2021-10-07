17934e750e50d4715fbf633bcb93df9f99ba704e
BrazhnikDA <65236303+BrazhnikDA@users.noreply.github.com>
2021-10-07 17:16:40 +0300

    Таблица вывода исправлена

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from prettytable import PrettyTable  # Для отображения таблиц в консоли
-from Information import Information
+from Information import Information  # Подключаем наш класс
 
+# Отображение таблицы в консоль, на вход подаётся обьект класса Information
 def CreateTable(info):
-    th = ['Наименование', 'Затраты', 'Описание']
+    th = ['Наименование', 'Количество', 'Стоимость/Затраты', 'Описание']
     td = \
         [
-            '1', '2', '3',
-            'b', '2', '3',
-            'c', '2', '3',
-            'd', 'ddddddddd', '33333333333333333333'
+            info.ListObject[0].Name, info.ListObject[0].Count, info.ListObject[0].Salary, info.ListObject[0].Description,
+            info.ListObject[1].Name, info.ListObject[1].Count, info.ListObject[1].Salary, info.ListObject[1].Description,
+            info.ListObject[2].Name, info.ListObject[2].Count, info.ListObject[2].Salary, info.ListObject[2].Description,
+            info.ListObject[3].Name, info.ListObject[3].Count, info.ListObject[3].Salary, info.ListObject[3].Description
         ]
 
     columns = len(th)
@@ -22,16 +23,21 @@ def CreateTable(info):
 
 def main():
     info = Information()
+    info.Start()
+
+    countLecturer = info.ListObject[0].Count
+    salaryLecturer = info.ListObject[0].Salary
 
     print("Попробуем подсчитать примерную стоимость обучения в вузе.")
     print("За 6 лет программы обучения нам потребуется (учитывая все симестры) изучить n предметов.")
-    print("Если взять, то что 1 преподаватель может вести 3 предмета, нам потруебся - ",
-          info.countLecturer, "преподавателей")
-    print("Средняя зарплата преподавателя по России - ", info.lecturerCost)
-    print("За 6 лет обучения это - ", info.lecturerCost * 12 * 6 * info.countLecturer,
-          "именно столько уйдёт на зарплату ", info.countLecturer, "преподавателям за 6 лет обучения")
+    print("Если взять, то что 1 преподаватель может вести 3 предмета, нам потруебся - ", countLecturer, "преподавателей")
+    print("Средняя зарплата преподавателя по России - ", salaryLecturer)
+    print("За 6 лет обучения это - ", salaryLecturer * 12 * 6 * countLecturer,
+          "именно столько уйдёт на зарплату ", countLecturer, "преподавателям за 6 лет обучения")
     print("Теперь подсчитаем остальные затраты при оффлайн обучении: ")
     CreateTable(info)
+
+    # На будущее
     # Построить график [x,y] где по x количество месяцев за 6 лет, по y сумма денег, нарисовать 2 линии потрачено и заработано при полном наборе предметов (онлайн,офлайн)
     # Добавить ещё 2 линии с тем сколько бы было потрачено, если бы предметы были только "нужные" по нашему мнению
 
