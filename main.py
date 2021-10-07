170c3fd529be313ed951ab715fd78a023077c461
BrazhnikDA <65236303+BrazhnikDA@users.noreply.github.com>
2021-10-07 18:39:28 +0300

    Для вывода написан цикл

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 from prettytable import PrettyTable  # Для отображения таблиц в консоли
+
 from Information import Information  # Подключаем наш класс
+
 
 # Отображение таблицы в консоль, на вход подаётся обьект класса Information
 def CreateTable(info):
     th = ['Наименование', 'Количество', 'Стоимость/Затраты', 'Описание']
-    td = \
-        [
-            info.ListObject[0].Name, info.ListObject[0].Count, info.ListObject[0].Salary, info.ListObject[0].Description,
-            info.ListObject[1].Name, info.ListObject[1].Count, info.ListObject[1].Salary, info.ListObject[1].Description,
-            info.ListObject[2].Name, info.ListObject[2].Count, info.ListObject[2].Salary, info.ListObject[2].Description,
-            info.ListObject[3].Name, info.ListObject[3].Count, info.ListObject[3].Salary, info.ListObject[3].Description
-        ]
+    td = []
+
+    for i in range(info.ListObject.__len__()):
+        td.append(info.ListObject[i].Name)
+        td.append(info.ListObject[i].Count)
+        td.append(info.ListObject[i].Salary)
+        td.append(info.ListObject[i].Description)
 
     columns = len(th)
     table = PrettyTable(th)
@@ -30,7 +32,8 @@ def main():
 
     print("Попробуем подсчитать примерную стоимость обучения в вузе.")
     print("За 6 лет программы обучения нам потребуется (учитывая все симестры) изучить n предметов.")
-    print("Если взять, то что 1 преподаватель может вести 3 предмета, нам потруебся - ", countLecturer, "преподавателей")
+    print("Если взять, то что 1 преподаватель может вести 3 предмета, нам потруебся - ", countLecturer,
+          "преподавателей")
     print("Средняя зарплата преподавателя по России - ", salaryLecturer)
     print("За 6 лет обучения это - ", salaryLecturer * 12 * 6 * countLecturer,
           "именно столько уйдёт на зарплату ", countLecturer, "преподавателям за 6 лет обучения")
